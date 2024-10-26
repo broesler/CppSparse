81f5fed517f55af4010487d4ad8db155da9b61e3
Bernard Roesler <bernard.roesler@gmail.com>
2024-10-25 21:13:19 -0400

    Create a makefile. Convert COOMatrix tests to Catch2 framework.

---
+++ makefile
@@ -0,0 +1,51 @@
+#==============================================================================
+#    File: makefile
+# Created: 2024-10-24 17:45
+#  Author: Bernie Roesler
+#
+#  Description: Build executables and tests for CSparse++.
+#==============================================================================
+
+# Set the compiler options
+CC = clang++
+CFLAGS = -Wall -pedantic -std=c++20
+
+# BREW := $(shell brew --prefix)  # FIXME adds space? run command only once
+BREW = /opt/homebrew
+
+INCL = $(wildcard *.h)
+SRC = $(wildcard *.cpp)
+OBJ = $(SRC:%.cpp=%.o)
+
+# -----------------------------------------------------------------------------
+#        Make options 
+# -----------------------------------------------------------------------------
+all: test_csparse
+
+test: OPT = -I$(BREW)/include 
+test: LDLIBS = -L$(BREW)/lib -lcatch2 -lCatch2Main
+test: test_csparse
+
+debug: CFLAGS += -DDEBUG -glldb -fno-inline -fsanitize=address,leak
+debug: all
+
+# -----------------------------------------------------------------------------
+#         Compile and Link
+# -----------------------------------------------------------------------------
+test_csparse: % : %.o $(OBJ)
+	$(CC) $(CFLAGS) $(OPT) -o $@ $^ $(LDLIBS)
+
+# Objects depend on source and headers
+%.o : %.cpp $(INCL)
+	$(CC) $(CFLAGS) $(OPT) -c $< -o $@
+
+# clean up
+.PHONY: depend clean
+clean:
+	rm -f *~
+	rm -f *.o
+	rm -rf *.dSYM/
+	rm -f test_csparse
+
+#==============================================================================
+#==============================================================================
--- test_csparse.cpp
+++ test_csparse.cpp
@@ -7,125 +7,194 @@
  *
  *============================================================================*/
 
-#include <cassert>
+#define CATCH_CONFIG_MAIN  // tell the compiler to define `main()`
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
+
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
 
 #include "csparse.h"
 
 using namespace std;
+using Catch::Matchers::Equals;
 
 
-int main(void)
+TEST_CASE("Test COOMatrix Constructors", "[COOMatrix]")
 {
-    COOMatrix A;
-    cout << "A has " << A.nnz() << " entries." << endl;
-    std::array<csint, 2> A_shape = A.shape();
-    cout << "A has shape (" << A_shape[0] << ", " << A_shape[1] << ")" << endl;
+    SECTION("Empty constructor") {
+        COOMatrix A;
 
+        REQUIRE(A.nnz() == 0);
+        REQUIRE(A.nzmax() == 0);
+        REQUIRE(A.shape() == std::array<csint, 2>{0, 0});
+    }
+
+    SECTION("Make new from given shape") {
+        COOMatrix A(56, 37);
+        REQUIRE(A.nnz() == 0);
+        REQUIRE(A.nzmax() == 0);
+        REQUIRE(A.shape() == std::array<csint, 2>{56, 37});
+    }
+
+    SECTION("Allocate new from shape and nzmax") {
+        int nzmax = 1e4;
+        COOMatrix A(56, 37, nzmax);
+        REQUIRE(A.nnz() == nzmax);
+        REQUIRE(A.nzmax() >= nzmax);
+        REQUIRE(A.shape() == std::array<csint, 2>{56, 37});
+    }
+
+}
+
+
+TEST_CASE("COOMatrix from (v, i, j) literals.", "[COOMatrix]")
+{
     // See Davis pp 7-8, Eqn (2.1)
     std::vector<csint>  i = {2,    1,    3,    0,    1,    3,    3,    1,    0,    2};
     std::vector<csint>  j = {2,    0,    3,    2,    1,    0,    1,    3,    0,    1};
     std::vector<double> v = {3.0,  3.1,  1.0,  3.2,  2.9,  3.5,  0.4,  0.9,  4.5,  1.7};
-    COOMatrix B(v, i, j);
-    cout << "B has " << B.nnz() << " entries." << endl;
-    cout << "B can hold " << B.nzmax() << " entries." << endl;
-    cout << "B has shape (" << B.shape()[0] << ", " << B.shape()[1] << ")" << endl;
+    COOMatrix A(v, i, j);
 
-    // Print the internals via getters
-    cout << "Printing B arrays..." << endl;
-    std::vector<csint> Brow = B.row();
-    std::vector<csint> Bcol = B.column();
-    std::vector<double> Bdata = B.data();
-    for (int k = 0; k < B.nnz(); k++) {
-        cout << "(" << Brow[k] << ", " << Bcol[k] << "): " << Bdata[k] << endl;
+    SECTION("Test attributes") {
+        REQUIRE(A.nnz() == 10);
+        REQUIRE(A.nzmax() >= 10);
+        REQUIRE(A.shape() == std::array<csint, 2>{4, 4});
+        REQUIRE_THAT(A.row(), Equals(i));
+        REQUIRE_THAT(A.column(), Equals(j));
+        REQUIRE_THAT(A.data(), Equals(v));
     }
 
-    cout << "done." << endl;
-    // Brow[0] = 99;  // allowed, but no effect on B since getters return a copy
+    SECTION("Test printing") {
+        std::stringstream s;
 
-    // Test printing
-    B.print();
-    B.print(cout, true);
-    cout << B;
+        SECTION("Print short") {
+            std::string expect = 
+                "<COOrdinate Sparse matrix\n"
+                "        with 10 stored elements and shape (4, 4)>\n";
 
-    // Assign an existing element
-    B.assign(3, 3, 56.0);
-    cout << B;
-    cout << "B has " << B.nnz() << " entries." << endl;
-    cout << "B can hold " << B.nzmax() << " entries." << endl;
-    cout << "B has shape (" << B.shape()[0] << ", " << B.shape()[1] << ")" << endl;
+            A.print(s);  // default verbose=false
 
-    // Assign a new element that changes the dimensions
-    B.assign(4, 3, 69.0);
-    cout << B;
-    cout << "B has " << B.nnz() << " entries." << endl;
-    cout << "B can hold " << B.nzmax() << " entries." << endl;
-    cout << "B has shape (" << B.shape()[0] << ", " << B.shape()[1] << ")" << endl;
+            REQUIRE(s.str() == expect);
+        }
 
-    // Tranpose
-    COOMatrix B_transpose = B.T();  // copy
-    cout << "B.T() by copy = " << endl << B_transpose;
-    assert (&B != &B_transpose);
+        SECTION("Print verbose") {
+            std::string expect =
+                "<COOrdinate Sparse matrix\n"
+                "        with 10 stored elements and shape (4, 4)>\n"
+                "(2, 2): 3\n"
+                "(1, 0): 3.1\n"
+                "(3, 3): 1\n"
+                "(0, 2): 3.2\n"
+                "(1, 1): 2.9\n"
+                "(3, 0): 3.5\n"
+                "(3, 1): 0.4\n"
+                "(1, 3): 0.9\n"
+                "(0, 0): 4.5\n"
+                "(2, 1): 1.7\n";
 
-    // Make new for given shape and nzmax
-    COOMatrix D(56, 37);
-    cout << "D = \n" << D;
-    cout << "D can hold " << D.nzmax() << " entries." << endl;
+            SECTION("Print from function") {
+                A.print(s, true);
+                REQUIRE(s.str() == expect);
+            }
 
-    COOMatrix E(56, 37, (int) 1e4);
-    cout << "E = \n" << E;
-    cout << "E can hold " << E.nzmax() << " entries." << endl;
+            SECTION("Print from operator<< overload") {
+                s << A;
+                REQUIRE(s.str() == expect);
+            }
+        }
+    }
 
-    // Read from a file
-    std::ifstream fp("./data/t1");
-    COOMatrix F(fp);
-    cout << "F = \n" << F;
+    SECTION("Assign an existing element") {
+        A.assign(3, 3, 56.0);
 
-    // Test conversion
-    B = COOMatrix(v, i, j);
-    cout << "B = \n" << B;
+        REQUIRE(A.nnz() == 11);
+        REQUIRE(A.nzmax() >= 11);
+        REQUIRE(A.shape() == std::array<csint, 2>{4, 4});
+        // TODO implement a private "search" function to ensure it exists?
+    }
 
-    CSCMatrix C = B.tocsc();
-    cout << "C = \n" << C;
+    SECTION("Assign a new element that changes the dimensions") {
+        A.assign(4, 3, 69.0);
 
-    // TODO test whether transpose, droptol, etc. change the original if we do
-    // an assignment
+        REQUIRE(A.nnz() == 11);
+        REQUIRE(A.nzmax() >= 11);
+        REQUIRE(A.shape() == std::array<csint, 2>{5, 4});
+    }
 
-    // Transpose
-    cout << "C.T = \n" << C.T();
+    SECTION("Tranpose") {
+        COOMatrix A_T = A.T();  // copy
 
-    // Sum Duplicates
-    B.assign(0, 2, 100.0);
-    B.assign(3, 0, 100.0);
-    B.assign(2, 1, 100.0);
-    C = B.tocsc().sum_duplicates();
-    cout << "C = \n" << C;
+        REQUIRE_THAT(A_T.row(), Equals(j));
+        REQUIRE_THAT(A_T.column(), Equals(i));
+        REQUIRE(&A != &A_T);
+    }
 
-    // Test droptol
-    C = COOMatrix(v, i, j).tocsc().droptol(2.0);
-    cout << "C = \n" << C;
+    SECTION("Read from a file") {
+        std::ifstream fp("./data/t1");
+        COOMatrix F(fp);
 
-    // TODO have assign return "*this" so we can chain assignments
-    // Test dropzeros
-    // C = COOMatrix(v, i, j)
-    //     .assign(0, 1, 0.0)
-    //     .assign(2, 1, 0.0)
-    //     .assign(3, 1, 0.0)
-    //     .tocsc();
+        // TODO implement A == F? Doesn't really work since we don't make any
+        // guarantees on the order of the elements. It would essentially just
+        // convert them to CSCMatrix, sort columns, and then compare.
+        REQUIRE(A.row() == F.row());
+        REQUIRE(A.column() == F.column());
+        REQUIRE(A.data() == F.data());
+    }
 
-    B = COOMatrix(v, i, j);
-    B.assign(0, 1, 0.0);
-    B.assign(2, 1, 0.0);
-    B.assign(3, 1, 0.0);
-    C = B.tocsc();
-
-    cout << "C with zeros = \n" << C;
-    C.dropzeros();
-    cout << "C without zeros = \n" << C;
-
-    return 0;
 }
+
+// TODO convert all of these into actual unit tests
+// int main(void)
+// {
+
+//     // Test conversion
+//     A = COOMatrix(v, i, j);
+//     cout << "A = \n" << A;
+
+//     CSCMatrix C = A.tocsc();
+//     cout << "C = \n" << C;
+
+//     // TODO test whether transpose, droptol, etc. change the original if we do
+//     // an assignment
+
+//     // Transpose
+//     cout << "C.T = \n" << C.T();
+
+//     // Sum Duplicates
+//     A.assign(0, 2, 100.0);
+//     A.assign(3, 0, 100.0);
+//     A.assign(2, 1, 100.0);
+//     C = A.tocsc().sum_duplicates();
+//     cout << "C = \n" << C;
+
+//     // Test droptol
+//     C = COOMatrix(v, i, j).tocsc().droptol(2.0);
+//     cout << "C = \n" << C;
+
+//     // TODO have assign return "*this" so we can chain assignments
+//     // Test dropzeros
+//     // C = COOMatrix(v, i, j)
+//     //     .assign(0, 1, 0.0)
+//     //     .assign(2, 1, 0.0)
+//     //     .assign(3, 1, 0.0)
+//     //     .tocsc();
+
+//     A = COOMatrix(v, i, j);
+//     A.assign(0, 1, 0.0);
+//     A.assign(2, 1, 0.0);
+//     A.assign(3, 1, 0.0);
+//     C = A.tocsc();
+
+//     cout << "C with zeros = \n" << C;
+//     C.dropzeros();
+//     cout << "C without zeros = \n" << C;
+
+//     return 0;
+// }
 
 /*==============================================================================
  *============================================================================*/

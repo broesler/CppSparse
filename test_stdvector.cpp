4c84059ab71aadbafde2c76683d84e264dfa7b8d
Bernard Roesler <bernard.roesler@gmail.com>
2024-12-18 11:54:34 -0500

    Add test for resize/shrink_to_fit larger.

--- test_stdvector.cpp
+++ test_stdvector.cpp
@@ -29,26 +29,43 @@ int main(void)
     cout << "v.size() = " << v.size() << endl;
     cout << "v.capacity() = " << v.capacity() << endl;
 
+    cout << "v.push_back(4)" << endl;
     v.push_back(4);
     print_vec(v);
     cout << "v.size() = " << v.size() << endl;  // == 4
     cout << "v.capacity() = " << v.capacity() << endl;  // == 6 auto doubles!!
-                                                        //
+
+    cout << "v.reserve(10)" << endl;
     v.reserve(10);
     print_vec(v);
     cout << "v.size() = " << v.size() << endl;
     cout << "v.capacity() = " << v.capacity() << endl;
 
+    cout << "v.push_back(5)" << endl;
     v.push_back(5);
     print_vec(v);
     cout << "v.size() = " << v.size() << endl;
     cout << "v.capacity() = " << v.capacity() << endl;
 
+    cout << "v.resize(3)" << endl;
     v.resize(3);
     print_vec(v);
     cout << "v.size() = " << v.size() << endl;
     cout << "v.capacity() = " << v.capacity() << endl;
 
+    cout << "v.shrink_to_fit()" << endl;
+    v.shrink_to_fit();
+    print_vec(v);
+    cout << "v.size() = " << v.size() << endl;
+    cout << "v.capacity() = " << v.capacity() << endl;
+
+    cout << "v.resize(7)" << endl;
+    v.resize(7);
+    print_vec(v);
+    cout << "v.size() = " << v.size() << endl;
+    cout << "v.capacity() = " << v.capacity() << endl;
+
+    cout << "v.shrink_to_fit()" << endl;
     v.shrink_to_fit();
     print_vec(v);
     cout << "v.size() = " << v.size() << endl;

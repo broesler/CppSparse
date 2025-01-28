ebcb5c25111acc91b032c0460f81f4245a230a68
Bernard Roesler <bernard.roesler@gmail.com>
2025-01-27 22:11:06 -0500

    Minor tweaks.

--- decomposition.cpp
+++ decomposition.cpp
@@ -7,7 +7,8 @@
  *
  *============================================================================*/
 
-#include <numeric>  // for std::iota
+#include <cmath>    // std::sqrt
+#include <numeric>  // std::iota
 
 #include "csparse.h"
 
@@ -76,38 +77,40 @@ CSCMatrix chol(const CSCMatrix& A, const Symbolic& S)
     std::vector<csint> c(S.cp);  // column pointers for L
     std::vector<double> x(N);    // sparse accumulator
 
-    // CSCMatrix C = S.p_inv.empty() ? A : A.symperm(S.p_inv);
-    CSCMatrix C = A.symperm(S.p_inv);  // just a copy if identity permutation
+    // const CSCMatrix C = S.p_inv.empty() ? A : A.symperm(S.p_inv);
+    const CSCMatrix C = A.symperm(S.p_inv);
 
     L.p_ = S.cp;  // column pointers for L
 
     // Compute L(:, k) for L*L' = C
     for (csint k = 0; k < N; k++) {
         //--- Nonzero pattern of L(:, k) ---------------------------------------
-        std::vector<csint> s = C.ereach(k, S.parent);  // find pattern of L(k, :)
-        x[k] = 0;                   // x(0:k) is now zero
+        const std::vector<csint> s = C.ereach(k, S.parent);  // pattern of L(k, :)
+        x[k] = 0.0;  // x(0:k) is now zero
 
-        // x = full(triu(C(:,k))) scatter into x
+        // scatter into x = full(triu(C(:,k)))
         for (csint p = C.p_[k]; p < C.p_[k+1]; p++) {
-            if (C.i_[p] <= k) {
-                x[C.i_[p]] = C.v_[p];
+            csint i = C.i_[p];
+            if (i <= k) {
+                x[i] = C.v_[p];
             }
         }
 
         double d = x[k];  // d = C(k, k)
-        x[k] = 0;  // clear x for k + 1st iteration
+        x[k] = 0.0;       // clear x for k + 1st iteration
 
         //--- Triangular Solve -------------------------------------------------
-        // Solve L[:k, :k] * x = C[:, k]
+        // Solve L(0:k-1, 0:k-1) * x = C(:, k)
         for (const auto& i : s) {
             double lki = x[i] / L.v_[L.p_[i]];  // L(k, i) = x(i) / L(i, i)
-            x[i] = 0;                           // clear x for k + 1st iteration
+            x[i] = 0.0;                         // clear x for k + 1st iteration
 
             for (csint p = L.p_[i] + 1; p < c[i]; p++) {
                 x[L.i_[p]] -= L.v_[p] * lki;    // x -= L(i, :) * L(k, i)
             }
 
             d -= lki * lki;                     // d -= L(k, i) * L(k, i)
+
             csint p = c[i]++;
             L.i_[p] = k;                        // store L(k, i) in column i
             L.v_[p] = lki;
@@ -120,10 +123,8 @@ CSCMatrix chol(const CSCMatrix& A, const Symbolic& S)
 
         csint p = c[k]++;
         L.i_[p] = k;  // store L(k, k) = sqrt(d) in column k
-        L.v_[p] = sqrt(d);
+        L.v_[p] = std::sqrt(d);
     }
-
-    L.p_[N] = S.cp[N];  // finalize column pointers
 
     return L;
 }

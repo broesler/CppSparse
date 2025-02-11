0fcdac0fbcf3b1017fde7e68ae4a83fb0d19016e
Bernard Roesler <bernard.roesler@gmail.com>
2025-02-11 12:07:50 -0500

    Add flag to use Laplacian matrix in chol_perf.

--- python/chol_perf.py
+++ python/chol_perf.py
@@ -22,43 +22,46 @@ from scipy.sparse.linalg import LaplacianNd
 import csparse as cs
 
 
+LAPLACE = True
 SAVE_FIG = True
 
 SEED = 565656
 
-filestem = 'chol_perf_py'
+if LAPLACE:
+    filestem = 'chol_perf_laplace'
+else:
+    filestem = 'chol_perf_random'
 
 # -----------------------------------------------------------------------------
 #         Create the data
 # -----------------------------------------------------------------------------
 chol_funcs = [cs.chol, cs.leftchol, cs.rechol]
 
-# Ns = [10, 100, 1000]
-Ns = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
-
-density = 0.2
+if LAPLACE:
+    # for Lapalacian, define sqrtN and N = sqrtN^2
+    Ns = [3, 4, 7, 10, 14, 22, 31, 44, 70]
+    N_cols = [N**2 for N in Ns]
+else:
+    Ns = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
+    N_cols = Ns
+    density = 0.2
 
 N_repeats = 3  # number of "runs" in %timeit (7 is default)
 N_samples = 1  # number of samples in each run (100,000 is default)
 
 times = defaultdict(list)
+fill_in = []
 
 rng = np.random.default_rng(SEED)
 
 for N in Ns:
-    print(f"---------- N = {N:6,d} ----------")
-    # for density in densities:
-    # print(f"---------- Density = {density:6.2g} ----------")
-
-    # TODO make this an option and change the filename output
-    # Use a non-random matrix like a 2D Laplacian
-    sqrtN = int(np.sqrt(N))
-    lap = LaplacianNd((sqrtN, sqrtN)).tosparse().tocsc()
-
-    A = cs.CSCMatrix(lap.data, lap.indices, lap.indptr, lap.shape)
-
-    # Create a random matrix
-    # A = cs.COOMatrix.random(N, N, density, SEED).tocsc()
+    if LAPLACE:
+        # Use a non-random matrix like a 2D Laplacian
+        lap = LaplacianNd((N, N)).tosparse().tocsc()
+        A = cs.CSCMatrix(lap.data, lap.indices, lap.indptr, lap.shape)
+    else:
+        # Create a random matrix
+        A = cs.COOMatrix.random(N, N, density, SEED).tocsc()
 
     # Ensure all diagonals are non-zero so that L is non-singular
     for i in range(A.shape()[0]):
@@ -66,6 +69,8 @@ for N in Ns:
 
     # Make sure the matrix is symmetric, positive definite
     A = A + A.T()
+
+    print(f"---------- N = {A.shape()[1]:6,d} ----------")
 
     # Get the symbolic factorization (same for all methods)
     S = cs.schol(A)
@@ -92,6 +97,11 @@ for N in Ns:
         print(f"{func_name}: {ts_min:.4g} s per loop, "
               f"({N_repeats} runs, {N_samples} loops each)")
 
+    # Compute fill-in
+    fill_in.append((L.nnz() - A.nnz()) / A.nnz())
+
+
+print(np.c_[Ns, fill_in])
 
 # -----------------------------------------------------------------------------
 #         Plot the data
@@ -99,7 +109,7 @@ for N in Ns:
 fig, ax = plt.subplots(num=1, clear=True)
 fig.set_size_inches(6.4, 4.8, forward=True)
 for i, (key, val) in enumerate(times.items()):
-    ax.plot(Ns, val, '.-', label=key)
+    ax.plot(N_cols, val, '.-', label=key)
 
 ax.set_xscale('log')
 ax.set_yscale('log')
@@ -108,12 +118,22 @@ ax.legend()
 
 ax.set_xlabel('Number of Columns')
 ax.set_ylabel('Time (s)')
-ax.set_title(f"{filestem.split('_')[0]}, density {density}")
+
+title = f"{filestem.split('_')[0]}"
+
+if not LAPLACE:
+    title += f", density {density}"
+
+ax.set_title(title)
 
 if SAVE_FIG:
-    fig_fullpath = Path(f"../plots/{filestem}_d{int(100*density):02d}.png")
+    fig_fullpath = f"../plots/{filestem}.png"
+
+    if not LAPLACE:
+        re.sub('\.png', f"_d{int(100*density):02d}.png", fig_fullpath)
+
     try:
-        fig.savefig(fig_fullpath)
+        fig.savefig(Path(fig_fullpath))
         print(f"Saved figure to {fig_fullpath}.")
     except Exception as e:
         print(f"Could not save figure to {fig_fullpath}: {e}")
@@ -121,12 +141,12 @@ if SAVE_FIG:
 
 
 # Plot the spy of each matrix to see fill-in
-fig, axs = plt.subplots(num=2, ncols=2, clear=True)
-Aa = np.array(A.toarray('C')).reshape(lap.shape)
-La = np.array(L.toarray('C')).reshape(lap.shape)
+# fig, axs = plt.subplots(num=2, ncols=2, clear=True)
+# Aa = np.array(A.toarray('C')).reshape(lap.shape)
+# La = np.array(L.toarray('C')).reshape(lap.shape)
 
-axs[0].spy(Aa)
-axs[1].spy(La)
+# axs[0].spy(Aa)
+# axs[1].spy(La)
 
 plt.show()
 

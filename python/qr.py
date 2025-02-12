38491aec66aa6ebf35516d05a8e3a4ec8e42f74b
Bernard Roesler <bernard.roesler@gmail.com>
2025-02-11 20:22:30 -0500

    Fix bug in python implementation. Add MATLAB qr_right.m for testing.

--- python/qr.py
+++ python/qr.py
@@ -23,6 +23,20 @@ tol = 1e-14
 def qr_right(A):
     """Compute the QR decomposition of A using the right-looking algorithm.
 
+    From the LAPACK documentation for `dgeqrf.f`:
+
+    The matrix Q is represented as a product of elementary reflectors
+
+       Q = H(1) H(2) . . . H(k), where k = min(m,n).
+
+    Each H(i) has the form
+
+       H(i) = I - tau * v * v**T
+
+    where tau is a real scalar, and v is a real vector with
+    v(1:i-1) = 0 and v(i) = 1; v(i+1:m) is stored on exit in A(i+1:m,i),
+    and tau in TAU(i).
+
     Parameters
     ----------
     A : (M, N) ndarray
@@ -44,9 +58,10 @@ def qr_right(A):
 
     for k in range(N):
         # Compute the Householder reflector
-        (v, b), _ = la.qr(R[k:, [k]], mode='raw')
-        b = b[0]  # singleton
-        V[k:, [k]] = v  # column vector
+        (Qraw, b), _ = la.qr(R[k:, [k]], mode='raw')
+        v = np.vstack([1.0, Qraw[1:]])  # extract the reflector
+        b = float(b.squeeze())          # get the scalar
+        V[k:, [k]] = v
         beta[k] = b
         R[k:, k:] -= v @ (b * (v.T @ R[k:, k:]))
 
@@ -81,7 +96,21 @@ def extract_householder_reflectors(Q):
 
 
 def construct_householder_matrix(v, tau=None):
-    """Constructs the Householder matrix from its defining vector."""
+    """Constructs the Householder matrix from its defining vector.
+
+    Parameters
+    ----------
+    v : (M,) array_like
+        A vector defining the Householder reflector.
+    tau : float, optional
+        The scaling factor for the reflector. If not provided, it is computed
+        as `2 / (v.T @ v)`.
+
+    Returns
+    -------
+    H : (M, N) ndarray
+        The Householder matrix.
+    """
     v = np.asarray(v, dtype=float)
     M = v.size
     v = v.reshape(M, 1)  # make it a column vector
@@ -90,6 +119,31 @@ def construct_householder_matrix(v, tau=None):
     return np.eye(M) - tau * (v @ v.T)
 
 
+def build_Q(V, beta):
+    """Construct the Q matrix from the Householder reflectors.
+
+    Parameters
+    ----------
+    V : (M, N) ndarray
+        A matrix with the Householder reflectors as columns.
+    beta : (N,) ndarray
+        A vector with the scaling factors for each reflector.
+
+    Returns
+    -------
+    Q : (M, N) ndarray
+        The orthogonal matrix Q.
+    """
+    M, N = V.shape
+    Q = np.eye(M)
+    for i in range(N):
+        Q @= construct_householder_matrix(V[:, i], beta[i])
+    return Q
+
+
+# -----------------------------------------------------------------------------
+#         Main Script
+# -----------------------------------------------------------------------------
 if __name__ == '__main__':
     # See: Strang Linear Algebra p 203.
     A = np.array([[1, 1, 2],
@@ -97,30 +151,36 @@ if __name__ == '__main__':
                   [1, 0, 0]],
                  dtype=float)
 
-    # Get the raw LAPACK output
+    # Get the raw LAPACK output for the entire matrix
     # Qc stores the Householder reflectors *below* the diagonal*
-    (Qc, tau), R_ = la.qr(A, mode='raw')
-
+    (Qc, tau), Rraw = la.qr(A, mode='raw')
     reflectors = extract_householder_reflectors(Qc)
+    Hs = [construct_householder_matrix(v, t) for v, t in zip(reflectors, tau)]
+    Qr = build_Q(np.array(reflectors).T, tau)
+    np.testing.assert_allclose(Qr @ Rraw, A, atol=tol)
+    
+    # Test our own QR decomposition
+    V, beta, R = qr_right(A)
 
-    Hs = [construct_householder_matrix(v, t)
-          for v, t in zip(reflectors, tau)]
+    # NOTE that V is scaled to have 1 on the diagonal, whereas the MATLAB
+    # output from `qr_right.m` is not.
+    print("V = ")
+    print(V)
+    print("beta = ")
+    print(beta)
+    print("R = ")
+    print(R)
 
-    Qr = np.eye(A.shape[0])
-    for v, t in zip(reflectors, tau):
-        Qr @= construct_householder_matrix(v, t)
+    Q = build_Q(V, beta)
 
-    np.testing.assert_allclose(Qr @ R_, A, atol=tol)
-
-    V_r, beta_r, R_r = qr_right(A)
-    print(V_r)
-    print(beta_r)
-    print(R_r)
+    # Reproduce A = QR
+    np.testing.assert_allclose(Q @ R, A, atol=tol)
 
     # Compare to scipy's QR
-    Q, R = la.qr(A)
+    Q_, R_ = la.qr(A)
+    np.testing.assert_allclose(Q_, Q)
+    np.testing.assert_allclose(R_, R)
 
-    # np.testing.assert_allclose(R_r, R)
 
 # =============================================================================
 # =============================================================================

e4acc4fd6160cff9ac75b14ead75d20b4d16c5ca
Bernard Roesler <bernard.roesler@gmail.com>
2025-02-17 13:02:47 -0500

    Convert Davis Eqn (2.1) matrix into a MATLAB function for ease of use.
    Update the qr_demo.m script to use it.

--- MATLAB/davis_example.m
+++ MATLAB/davis_example.m
@@ -1,10 +1,27 @@
+function A = davis_example()
+% DAVIS_EXAMPLE Creates a sparse matrix from Davis pp 7-8, Eqn (2.1).
+%
+% Example matrix from Davis, "Direct Methods for Sparse Linear Systems", p. 7-8,
+% Eqn (2.1).
+%
+% Inputs:
+%   None
+% Outputs:
+%   A - (sparse) matrix:
+%       [[4.5, 0. , 3.2, 0. ],
+%        [3.1, 2.9, 0. , 0.9],
+%        [0. , 1.7, 3. , 0. ],
+%        [3.5, 0.4, 0. , 1. ]]
+%
+% Example:
+%  A = davis_example();
+%
 %===============================================================================
 %     File: davis_example.m
 %  Created: 2025-02-05 11:43
 %   Author: Bernie Roesler
 %
 %  Description: Davis matrix example Equation (2.1).
-%
 %===============================================================================
 
 % See Davis pp 7-8, Eqn (2.1)
@@ -14,28 +31,6 @@ v = [3.0,  3.1,  1.0,  3.2,  2.9,  3.5,  0.4,  0.9,  4.5,  1.7];
 
 A = sparse(i, j, v, 4, 4);
 
-% Compute the QR decomposition two different ways
-[V_r, Beta_r, R_r] = qr_right(A);
-[V_l, Beta_l, R_l] = qr_left(A);
-
-% V_r and V_l are the same,
-% Beta_r and Beta_l are the same
-% R_r and R_l are different, but only on the diagonal:
-%  R(1, 1) differs by a sign. The rest have the same sign but different values.
-
-% Compute the Q matrix by applying the Householder vectors to the identity
-Q_r = cs_qright(V_r, Beta_r, [], speye(size(V_r, 1)));
-Q_l = cs_qright(V_l, Beta_l, [], speye(size(V_l, 1)));
-
-[Q_, R_] = qr(A);
-
-% Q_r and Q_l are the same
-% Q_ differs from Q_r and Q
-
-% Compare to the built-in MATLAB QR decomposition
-norm(abs(Q_r*R_r - A), 2)
-norm(abs(Q_l*R_l - A), 2)  % FIXME fails
-norm(abs(Q_*R_ - A), 2)
-
+end
 %===============================================================================
 %===============================================================================
--- MATLAB/qr_demo.m
+++ MATLAB/qr_demo.m
@@ -7,16 +7,40 @@
 %
 %===============================================================================
 
-% See Strang "Linear Algebra", p. 203
-A = [[1 1 2]
-     [0 0 1]
-     [1 0 0]];
+% % See Strang "Linear Algebra", p. 203
+% A = [[1 1 2]
+%      [0 0 1]
+%      [1 0 0]];
 
-[V_r, Beta_r, R_r] = qr_right(A)
-
-[V_l, Beta_l, R_l] = qr_left(A)
+% [V_r, Beta_r, R_r] = qr_right(A)
+% [V_l, Beta_l, R_l] = qr_left(A)
 
 % [Q, R] = qr(A);
+
+A = davis_example();
+
+% Compute the QR decomposition two different ways
+[V_r, Beta_r, R_r] = qr_right(A);
+[V_l, Beta_l, R_l] = qr_left(A);  % FIXME this function is incorrect
+
+% V_r and V_l are the same,
+% Beta_r and Beta_l are the same
+% R_r and R_l are different, but only on the diagonal:
+%  R(1, 1) differs by a sign. The rest have the same sign but different values.
+
+% Compute the Q matrix by applying the Householder vectors to the identity
+Q_r = cs_qright(V_r, Beta_r, [], speye(size(V_r, 1)));
+Q_l = cs_qright(V_l, Beta_l, [], speye(size(V_l, 1)));
+
+[Q_, R_] = qr(A);
+
+% Q_r and Q_l are the same
+% Q_ differs from Q_r and Q
+
+% Compare to the built-in MATLAB QR decomposition
+norm(abs(Q_r * R_r - A), 2)
+norm(abs(Q_l * R_l - A), 2)  % FIXME fails
+norm(abs(Q_ * R_ - A), 2)
 
 
 %===============================================================================

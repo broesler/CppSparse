7c24489bc0c3365087e54d8e09bdd582e71e12f5
Bernard Roesler <bernard.roesler@gmail.com>
2025-02-17 14:41:31 -0500

    Extract V from Qraw for comparison. Does not match cs::qr.

--- python/scripts/qr_example.py
+++ python/scripts/qr_example.py
@@ -23,12 +23,21 @@ Ac = csparse.davis_example()
 A = csparse.to_scipy_sparse(Ac)
 N = A.shape[0]
 
+A_dense = A.toarray()
+
 # Compute the QR decomposition of A with scipy
-(Qraw, tau), Rraw = la.qr(A.toarray(), mode='raw')
-Q_, R_ = la.qr(A.toarray())
+(Qraw, tau), Rraw = la.qr(A_dense, mode='raw')
+Q_, R_ = la.qr(A_dense)
 
 atol = 1e-15
 np.testing.assert_allclose(Q_ @ R_, A.toarray(), atol=atol)
+
+V_ = np.tril(Qraw, -1) + np.eye(N)
+Qr_ = csparse.qright(V_, tau)
+Ql_ = csparse.qleft(V_, tau)
+
+np.testing.assert_allclose(Qr_, Ql_.T, atol=atol)
+np.testing.assert_allclose(Qr_, Q_, atol=atol)
 
 # Compute QR decomposition with csparse
 S = csparse.sqr(Ac)
@@ -36,17 +45,16 @@ VbR = csparse.qr(Ac, S)
 
 V, beta, Rraw = VbR.V, VbR.beta, VbR.R
 
+print("V = ")
+print(csparse.to_ndarray(V))
+print("beta = ")
+print(np.r_[beta])
 print("Rraw = ")
 print(csparse.to_ndarray(Rraw))
 
 # Get the actual Q matrix
 Qr = csparse.qright(V, beta)
-print("Qr = ")
-print(Qr.toarray())
-
 Ql = csparse.qleft(V, beta)
-print("Ql = ")
-print(Ql.toarray())
 
 np.testing.assert_allclose(Qr.toarray(), Ql.T.toarray(), atol=atol)
 

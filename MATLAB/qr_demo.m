a8e6afbf37e9d522e615e09532d6e0b8902f3053
Bernard Roesler <bernard.roesler@gmail.com>
2025-02-18 11:43:00 -0500

    Compute QR (V, beta, R) with both CSparse and python methods.

--- MATLAB/qr_demo.m
+++ MATLAB/qr_demo.m
@@ -7,23 +7,38 @@
 %
 %===============================================================================
 
-% % See Strang "Linear Algebra", p. 203
+tol = 1e-14;
+
+% ---------- See Strang "Linear Algebra", p. 203
+%   The Strang example has a worked QR decomposition for comparison.
 % A = [[1 1 2]
 %      [0 0 1]
 %      [1 0 0]];
-
 % [V_r, Beta_r, R_r] = qr_right(A)
 % [V_l, Beta_l, R_l] = qr_left(A)
-
 % [Q, R] = qr(A);
 
-A = davis_example();
+% ---------- Matrix from Davis Figure 5.1, p 74.
+N = 8
+rows = 1 + [0, 1, 2, 3, 4, 5, 6, 7, 3, 6, 1, 6, 0, 2, 5, 7, 4, 7, 0, 1, 3, 7, 5, 6];
+cols = 1 + [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 6, 6, 7, 7];
+vals = [1:N-1, 0, ones(1, size(rows, 2) - N)];
 
-% TODO make these edits in my branch of CSparse and remove the files from this
-% CSparse++ repo.
-% Compute the QR decomposition two different ways
+A = sparse(rows, cols, vals, N, N);
+
+% ---------- Take the example from Davis Eqn (2.1)
+% A = davis_example();
+
+%-------------------------------------------------------------------------------
+%        Compute the QR decomposition
+%-------------------------------------------------------------------------------
 [V_r, Beta_r, R_r] = qr_right(A);
 [V_l, Beta_l, R_l] = qr_left(A);  % FIXME R_l diagonal is not correct
+
+% Compute the QR decomposition using the CSparse implementation
+% NOTE that the CSparse implementation computes a row permutation:
+%   QR = PA
+[V, Beta, p, R] = cs_qr(A);
 
 % V is not scaled to be 1 on the diagonal, like in scipy.linalg.qr, so scale it
 % manually.
@@ -38,21 +53,33 @@ for k = 1:size(V_r, 2)
     V_lkk = V_l(k, k);
     V_l(:, k) /= V_lkk;
     Beta_l(k) *= V_lkk^2;
+
+    Vkk = V(k, k);
+    V(:, k) /= Vkk;
+    Beta(k) *= Vkk^2;
 end
 
-% V_r and V_l now matche the V from scipy.linalg.qr(..., mode'raw').
+% V_r and V_l now match the V from scipy.linalg.qr(..., mode'raw').
+assert(norm(abs(V_r - V_l)) < tol);
+assert(norm(abs(Beta_r - Beta_l)) < tol);
+% assert(norm(abs(R_r - R_l)) < tol);  % NOT true, since R_l is not correct
 
 % Compute the Q matrix by applying the Householder vectors to the identity
 Q_r = cs_qright(V_r, Beta_r, [], speye(size(V_r, 1)));
 Q_l = cs_qleft(V_l, Beta_l, [], speye(size(V_l, 1)));
 
+% Make sure to include the row permutation p!
+Q = cs_qright(V, Beta, p, speye(size(V, 1)));
+
+% Compute the QR decomposition using the built-in MATLAB function
 [Q_, R_] = qr(A);
 
 % Compare to the built-in MATLAB QR decomposition
-norm(abs(Q_r - Q_l'), 2)
-norm(abs(Q_r * R_r - A), 2)
-norm(abs(Q_l' * R_l - A), 2)  % FIXME fails
-norm(abs(Q_ * R_ - A), 2)
+norm(abs(Q_r - Q_l'))
+norm(abs(Q_r * R_r - A))
+norm(abs(Q_l' * R_l - A))  % FIXME fails since qr_left is wrong
+norm(abs(Q * R - A))
+norm(abs(Q_ * R_ - A))
 
 
 %===============================================================================
--- python/scripts/qr_example.py
+++ python/scripts/qr_example.py
@@ -18,10 +18,29 @@ from scipy import linalg as la
 
 import csparse
 
+# TODO move cholesky.py and qr.py functions into csparse.linalg module
+import sys
+sys.path.append('./')
 
-Ac = csparse.davis_example()
-A = csparse.to_scipy_sparse(Ac)
-N = A.shape[0]
+from qr import qr_right
+
+atol = 1e-15
+
+
+# ---------- Matrix from Davis Figure 5.1, p 74.
+N = 8
+rows = np.r_[0, 1, 2, 3, 4, 5, 6, 7, 3, 6, 1, 6, 0, 2, 5, 7, 4, 7, 0, 1, 3, 7, 5, 6];
+cols = np.r_[0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 6, 6, 7, 7];
+vals = np.r_[np.arange(1, N), 0, np.ones(rows.size - N)]
+
+A = sparse.csc_array((vals, (rows, cols)), shape=(N, N))
+
+Ac = csparse.COOMatrix(vals, rows, cols, (N, N)).tocsc()
+
+# ---------- Davis 4x4 example
+# Ac = csparse.davis_example()
+# A = csparse.to_scipy_sparse(Ac)
+# N = A.shape[0]
 
 A_dense = A.toarray()
 
@@ -31,7 +50,6 @@ A_dense = A.toarray()
 (Qraw, tau), Rraw = la.qr(A_dense, mode='raw')
 Q_, R_ = la.qr(A_dense)
 
-atol = 1e-15
 np.testing.assert_allclose(Q_ @ R_, A.toarray(), atol=atol)
 
 V_ = np.tril(Qraw, -1) + np.eye(N)
@@ -42,24 +60,41 @@ np.testing.assert_allclose(Qr_, Ql_.T, atol=atol)
 np.testing.assert_allclose(Qr_, Q_, atol=atol)
 
 # -----------------------------------------------------------------------------
+#         Compute the QR decomposition with my python implementation
+# -----------------------------------------------------------------------------
+V_r, beta_r, R_r = qr_right(A.toarray())
+Q_r = csparse.qright(V_r, beta_r)
+
+# NOTE that the V, beta, and R results from scipy and my implementation are
+# different, but they are self-consistent and reproduce the original matrix.
+np.testing.assert_allclose(Q_r @ R_r, A.toarray(), atol=atol)
+
+# -----------------------------------------------------------------------------
 #         Compute QR decomposition with csparse
 # -----------------------------------------------------------------------------
+# TODO the row and column permutations are stored in S, but the MATLAB cs_qr
+# function returns p and q, and only uses S internally. We should implement 
+# this functionality in our csparse implementation.
 S = csparse.sqr(Ac)
-VbR = csparse.qr(Ac, S)
+QRres = csparse.qr(Ac, S)
 
-V, beta, R = VbR.V, VbR.beta, VbR.R
+# FIXME these matrices all seem wrong
+V, beta, R = QRres.V, QRres.beta, QRres.R
 
 R = csparse.to_scipy_sparse(R)
 
-np.testing.assert_allclose(V.toarray(), csparse.to_ndarray(V), atol=atol)
-np.testing.assert_allclose(V.toarray(order='F'), csparse.to_ndarray(V), atol=atol)
-np.testing.assert_allclose(V.toarray(order='C'), csparse.to_ndarray(V), atol=atol)
-np.testing.assert_allclose(V.tocoo().toarray(), csparse.to_ndarray(V), atol=atol)
-
-print("V.toarray() = ")
+print("V_ = ")
+print(V_)
+print("V = ")
 print(V.toarray())
+
+print("tau = ")
+print(np.r_[tau])
 print("beta = ")
 print(np.r_[beta])
+
+print("R_ = ")
+print(R_)
 print("R = ")
 print(R.toarray())
 

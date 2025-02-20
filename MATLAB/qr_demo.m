8e6a92a60c2213ecce3db39bade590cc47c6bb33
Bernard Roesler <bernard.roesler@gmail.com>
2025-02-20 09:04:30 -0500

    Update qr_demo.m with fixed bug in qr_left.m

--- MATLAB/qr_demo.m
+++ MATLAB/qr_demo.m
@@ -33,7 +33,7 @@ A = sparse(rows, cols, vals, N, N);
 %        Compute the QR decomposition
 %-------------------------------------------------------------------------------
 [V_r, Beta_r, R_r] = qr_right(A);
-[V_l, Beta_l, R_l] = qr_left(A);  % FIXME R_l diagonal is not correct
+[V_l, Beta_l, R_l] = qr_left(A);
 
 % Compute the QR decomposition using the CSparse implementation
 % NOTE that the CSparse implementation computes a row permutation:
@@ -62,7 +62,7 @@ end
 % V_r and V_l now match the V from scipy.linalg.qr(..., mode'raw').
 assert(norm(abs(V_r - V_l)) < tol);
 assert(norm(abs(Beta_r - Beta_l)) < tol);
-% assert(norm(abs(R_r - R_l)) < tol);  % NOT true, since R_l is not correct
+assert(norm(abs(R_r - R_l)) < tol);
 
 % NOTE that V != V_r, because of the row permutation applied
 % R == -R_r *except* for the last element R(N, N) == R_r(N, N)
@@ -74,17 +74,15 @@ Q_l = cs_qleft(V_l, Beta_l, [], speye(size(V_l, 1)));
 % Make sure to include the row permutation p!
 Q = cs_qright(V, Beta, p, speye(size(V, 1)));
 
-% QR = cs_qleft(V, Beta, p, R);  % fails?
-
 % Compute the QR decomposition using the built-in MATLAB function
 [Q_, R_] = qr(A);
 
 % Compare to the built-in MATLAB QR decomposition
 assert(norm(abs(Q_r - Q_l')) < tol);
 assert(norm(abs(Q_r * R_r - A)) < tol);
-% assert(norm(abs(Q_l' * R_l - A)) < tol);  % FIXME fails since qr_left is wrong
+assert(norm(abs(Q_l' * R_l - A)) < tol);
 assert(norm(abs(Q * R - A)) < tol);
-assert(norm(abs(Q_ * R_ - A)) < tol);
+assert(norm(abs(Q_ * R_ - A)) < tol);  % built-in
 
 
 %===============================================================================

6d55bd48332c29dc249b049ce6fa34274d384816
Bernard Roesler <bernard.roesler@gmail.com>
2025-02-21 08:37:05 -0500

    Organize west0479 experiment. COLAMD does not match MATLAB.

--- python/scripts/west0479_experiment.py
+++ python/scripts/west0479_experiment.py
@@ -21,7 +21,7 @@ from pathlib import Path
 #         Load the Data
 # -----------------------------------------------------------------------------
 data_path = Path('../../data/')
-filename = Path('west0479')  # TODO change indices in file to 0-based
+filename = Path('west0479')
 full_path = data_path / filename
 
 dtypes = np.dtype([
@@ -39,14 +39,23 @@ assert A.shape[0] == A.shape[1] == 479
 # -----------------------------------------------------------------------------
 #         Compute the QR decomposition of A with scipy
 # -----------------------------------------------------------------------------
-# Use LU to get the permutation vector
+# Use LU to get the permutation vector?
 # (inefficient! should have a dedicated colamd interface)
-# 'NATURAL', 'MMD_ATA', ''MMD_AT_PLUS_A', 'COLAMD'
-permc_spec = 'NATURAL'
-lu = spla.splu(A, permc_spec=permc_spec)
-q = lu.perm_c
+# 'NATURAL', 'MMD_ATA', 'MMD_AT_PLUS_A', 'COLAMD', 'RCM'
 
-Aq = A[:, q].toarray()
+permc_spec = 'COLAMD'
+
+match permc_spec:
+    case 'NATURAL' | 'MMD_ATA' | 'MMD_AT_PLUS_A' | 'COLAMD':
+        lu = spla.splu(A, permc_spec=permc_spec)
+        p = lu.perm_r
+        q = lu.perm_c
+    case 'RCM':
+        p = slice(None)
+        q = sparse.csgraph.reverse_cuthill_mckee(A)
+
+# Permute the matrix
+Aq = A[p][:, q].toarray()
 
 # Get the QR decomposition
 Q, R = la.qr(Aq)
@@ -54,21 +63,22 @@ Q, R = la.qr(Aq)
 # Get the Householder vectors
 (Qraw, tau), _ = la.qr(Aq, mode='raw')
 V = np.tril(Qraw, -1) + np.eye(Aq.shape[0])
-R_ = np.triu(Qraw)
 
-np.testing.assert_allclose(R, R_)
+# Convert to sparse matrices
+Q = sparse.csc_matrix(Q)
+V = sparse.csc_matrix(V)
+R = sparse.csc_matrix(R)
 
 # TODO check that these are correct? Book states "Q 38,070 vs V 3,906"
 # It seems like COLAMD is not working correctly. The householder_explicit.m
 # file computes nnz values that are similar to the book with q = colamd(A).
-print(f"{permc_spec = }")
+print(f"{permc_spec=}")
 # print(f"density of A: {A.nnz / (A.shape[0] * A.shape[1]):.4g}")
 # print("A.nnz:", A.nnz)
 
-print("Q.nnz:", sparse.csc_matrix(Q).nnz)
-
-print("V.nnz:", sparse.csc_matrix(V).nnz)
-print("R.nnz:", sparse.csc_matrix(R).nnz)
+print("Q.nnz:", Q.nnz)
+print("V.nnz:", V.nnz)
+print("R.nnz:", R.nnz)
 
 # -----------------------------------------------------------------------------
 #         Plots
@@ -80,6 +90,7 @@ fig.suptitle(f"{permc_spec} Ordering")
 for ax, M, title in zip(axs.flat, [A, V, Q, R], ['A', 'V', 'Q', 'R']):
     ax.spy(M, markersize=1)
     ax.set_title(title)
+    ax.set_xlabel(f"nnz = {M.nnz}")
 
 plt.show()
 

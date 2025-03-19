886d76b2019d8edb9e26391eaf4caaccf0fd4a35
Bernard Roesler <bernard.roesler@gmail.com>
2025-03-19 16:43:02 -0400

    Implement lu_right in python.

--- python/csparse/_lu.py
+++ python/csparse/_lu.py
@@ -28,19 +28,22 @@ def lu_left(A):
 
     Returns
     -------
+    P : (M, M) ndarray
+        Row permutation matrix.
     L : (M, M) ndarray
         Lower triangular matrix. The diagonal elements are all 1.
     U : (M, M) ndarray
         Upper triangular matrix.
-    P : (M, M) ndarray
-        Row permutation matrix.
     """
     M, N = A.shape
+
     if M != N:
         raise ValueError("Input matrix must be square.")
+
     P = np.eye(N)
     L = np.zeros((N, N))
     U = np.zeros((N, N))
+
     for k in range(N):
         if k == 0:
             x = P @ A[:, k]  # no need to solve for the first column
@@ -57,25 +60,66 @@ def lu_left(A):
         L[k, k] = 1
         L[k+1:, k] = x[k+1:] / x[k]  # divide the column by the pivot
 
-    return L, U, P
+    return P, L, U
+
+
+def lu_right(A):
+    """Compute the LU decomposition of a matrix using a right-looking algorithm
+    without pivoting.
+
+    .. math::
+        PA = LU
+
+    Parameters
+    ----------
+    A : (M, M) array_like
+        Square matrix to decompose.
+
+    Returns
+    -------
+    P : (M, M) ndarray
+        Row permutation matrix (identity matrix).
+    L : (M, M) ndarray
+        Lower triangular matrix. The diagonal elements are all 1.
+    U : (M, M) ndarray
+        Upper triangular matrix.
+    """
+    A = np.copy(A)
+    M, N = A.shape
+
+    if M != N:
+        raise ValueError("Input matrix must be square.")
+
+    L = np.eye(N)
+    U = np.zeros((N, N))
+
+    for k in range(N):
+        U[k, k:] = A[k, k:]
+        L[k+1:, k] = A[k+1:, k] / U[k, k]
+        A[k+1:, k+1:] -= L[k+1:, [k]] @ U[[k], k+1:]
+
+    return np.eye(N), L, U
+
 
 
 if __name__ == "__main__":
     import utils
     A = utils.davis_example_qr(format='ndarray')
 
-    # Computes LU = PA
-    L, U, P = lu_left(A)
-
     # Compute using scipy (computes A = PLU -> LU = P^T A)
     P_, L_, U_ = la.lu(A)
 
-    # Check the results
     atol = 1e-15
-    np.testing.assert_allclose(P, P_.T, atol=atol)
-    np.testing.assert_allclose(L, L_, atol=atol)
-    np.testing.assert_allclose(U, U_, atol=atol)
-    np.testing.assert_allclose(L @ U, P @ A, atol=atol)
+
+    for lu_func in [lu_left, lu_right]:
+        # Computes LU = PA
+        P, L, U = lu_func(A)
+
+        # Check the results
+        np.testing.assert_allclose(P, P_.T, atol=atol)
+        np.testing.assert_allclose(L, L_, atol=atol)
+        np.testing.assert_allclose(U, U_, atol=atol)
+        np.testing.assert_allclose(L @ U, P @ A, atol=atol)
 
 
 # =============================================================================

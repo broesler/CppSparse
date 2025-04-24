7f1bd210605e3a81b3ab373d385c6a7ef08e2c11
Bernard Roesler <bernard.roesler@gmail.com>
2025-04-23 22:35:15 -0400

    Update binding for davis_example_qr with add_diag argument.

--- src/pybind11_wrapper.cpp
+++ src/pybind11_wrapper.cpp
@@ -408,7 +408,7 @@ PYBIND11_MODULE(csparse, m) {
     // -------------------------------------------------------------------------
     m.def("_davis_example_small", &cs::davis_example_small);
     m.def("_davis_example_chol", &cs::davis_example_chol);
-    m.def("_davis_example_qr", &cs::davis_example_qr);
+    m.def("_davis_example_qr", &cs::davis_example_qr, py::arg("add_diag")=0.0);
     m.def("_davis_example_amd", &cs::davis_example_amd);
 
     //--------------------------------------------------------------------------

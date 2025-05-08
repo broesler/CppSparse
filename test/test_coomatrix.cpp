ff4486dfd3f58fcaf4ec818f5eb09c6ef7bda6b2
Bernard Roesler <bernard.roesler@gmail.com>
2025-05-08 12:02:37 -0400

    Separte COOMatrix tests to own file.

---
+++ test/test_coomatrix.cpp
@@ -0,0 +1,202 @@
+/*==============================================================================
+ *     File: test_coomatrix.cpp
+ *  Created: 2025-05-08 11:38
+ *   Author: Bernie Roesler
+ *
+ *  Description: Test COOMatrix constructors and other basic functions.
+ *
+ *============================================================================*/
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "csparse.h"
+#include "test_helpers.h"
+
+namespace cs {
+
+
+TEST_CASE("COOMatrix Constructors", "[COOMatrix][constructor]")
+{
+    SECTION("Empty constructor") {
+        COOMatrix A;
+
+        CHECK(A.nnz() == 0);
+        CHECK(A.nzmax() == 0);
+        CHECK(A.shape() == Shape {0, 0});
+    }
+
+    SECTION("Make new from given shape") {
+        COOMatrix A {{56, 37}};
+        CHECK(A.nnz() == 0);
+        CHECK(A.nzmax() == 0);
+        CHECK(A.shape() == Shape {56, 37});
+    }
+
+    SECTION("Allocate new from shape and nzmax") {
+        int nzmax = 1e4;
+        COOMatrix A {{56, 37}, nzmax};
+        CHECK(A.nnz() == 0);
+        CHECK(A.nzmax() >= nzmax);
+        CHECK(A.shape() == Shape {56, 37});
+    }
+
+    SECTION("From (v, i, j) literals") {
+        // See Davis pp 7-8, Eqn (2.1)
+        std::vector<csint>  i = {2,    1,    3,    0,    1,    3,    3,    1,    0,    2};
+        std::vector<csint>  j = {2,    0,    3,    2,    1,    0,    1,    3,    0,    1};
+        std::vector<double> v = {3.0,  3.1,  1.0,  3.2,  2.9,  3.5,  0.4,  0.9,  4.5,  1.7};
+        COOMatrix A {v, i, j};
+
+        CHECK(A.nnz() == 10);
+        CHECK(A.nzmax() >= 10);
+        CHECK(A.shape() == Shape{4, 4});
+        CHECK(A.row() == i);
+        CHECK(A.column() == j);
+        CHECK(A.data() == v);
+    }
+}
+
+
+TEST_CASE("COOMatrix methods", "[COOMatrix][methods]")
+{
+    COOMatrix A = davis_example_small();
+
+    SECTION("Printing") {
+        std::stringstream s;
+
+        SECTION("Print short") {
+            std::string expect =
+                "<COOrdinate Sparse matrix\n"
+                "        with 10 stored elements and shape (4, 4)>\n";
+
+            A.print(s);  // default verbose=false
+
+            REQUIRE(s.str() == expect);
+        }
+
+        SECTION("Print verbose") {
+            std::string expect =
+                "<COOrdinate Sparse matrix\n"
+                "        with 10 stored elements and shape (4, 4)>\n"
+                "(2, 2): 3\n"
+                "(1, 0): 3.1\n"
+                "(3, 3): 1\n"
+                "(0, 2): 3.2\n"
+                "(1, 1): 2.9\n"
+                "(3, 0): 3.5\n"
+                "(3, 1): 0.4\n"
+                "(1, 3): 0.9\n"
+                "(0, 0): 4.5\n"
+                "(2, 1): 1.7\n";
+
+            SECTION("Print from function") {
+                A.print(s, true);
+                REQUIRE(s.str() == expect);
+            }
+
+            SECTION("Print from operator<< overload") {
+                s << A;
+                REQUIRE(s.str() == expect);
+            }
+        }
+
+        // Clear the stringstream to prevent memory leaks
+        s.str("");
+        s.clear();
+    }
+
+    SECTION("Assign an existing element to create a duplicate") {
+        A.assign(3, 3, 56.0);
+
+        REQUIRE(A.nnz() == 11);
+        REQUIRE(A.nzmax() >= 11);
+        REQUIRE(A.shape() == Shape{4, 4});
+        // REQUIRE_THAT(A(3, 3), WithinAbs(57.0, tol));
+    }
+
+    SECTION("Assign a new element that changes the dimensions") {
+        A.assign(4, 3, 69.0);
+
+        REQUIRE(A.nnz() == 11);
+        REQUIRE(A.nzmax() >= 11);
+        REQUIRE(A.shape() == Shape{5, 4});
+        // REQUIRE_THAT(A(4, 3), WithinAbs(69.0, tol));
+    }
+
+    SECTION("Exercise 2.5: Assign a dense submatrix") {
+        std::vector<csint> rows = {2, 3, 4};
+        std::vector<csint> cols = {4, 5, 6};
+        std::vector<double> vals = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        A.assign(rows, cols, vals);
+
+        REQUIRE(A.nnz() == 19);
+        REQUIRE(A.nzmax() >= 19);
+        REQUIRE(A.shape() == Shape{5, 7});
+    }
+
+    SECTION("Exercise 2.6: Tranpose") {
+        COOMatrix A_T = A.transpose();
+        COOMatrix A_TT = A.T();
+
+        REQUIRE(A_T.row() == A.column());
+        REQUIRE(A_T.column() == A.row());
+        REQUIRE(A_T.row() == A_TT.row());
+        REQUIRE(A_T.column() == A_TT.column());
+        REQUIRE(&A != &A_T);
+    }
+
+    SECTION("Read from a file") {
+        std::ifstream fp("./data/t1");
+        COOMatrix F {fp};  // TODO refactor to use a static fromfile function
+
+        REQUIRE(A.row() == F.row());
+        REQUIRE(A.column() == F.column());
+        REQUIRE(A.data() == F.data());
+    }
+
+    SECTION("Conversion to dense array: Column-major") {
+        std::vector<double> expect = {
+            4.5, 3.1, 0.0, 3.5,
+            0.0, 2.9, 1.7, 0.4,
+            3.2, 0.0, 3.0, 0.0,
+            0.0, 0.9, 0.0, 1.0
+        };
+
+        REQUIRE(A.to_dense_vector() == expect);
+        REQUIRE(A.to_dense_vector('F') == expect);
+    }
+
+    SECTION("Conversion to dense array: Row-major") {
+        std::vector<double> expect = {
+            4.5, 0.0, 3.2, 0.0,
+            3.1, 2.9, 0.0, 0.9,
+            0.0, 1.7, 3.0, 0.0,
+            3.5, 0.4, 0.0, 1.0
+        };
+
+        REQUIRE(A.to_dense_vector('C') == expect);
+    }
+
+    SECTION("Generate random matrix") {
+        double density = 0.25;
+        csint M = 5, N = 10;
+        unsigned int seed = 56;  // seed for reproducibility
+        COOMatrix A = COOMatrix::random(M, N, density, seed);
+
+        REQUIRE(A.shape() == Shape{M, N});
+        REQUIRE(A.nnz() == (csint)(density * M * N));
+    }
+}
+
+
+}  // namespace cs
+
+/*==============================================================================
+ *============================================================================*/

f4b39de024a10bb5fe52f46aae5060d3b5ae52c6
Bernard Roesler <bernard.roesler@gmail.com>
2025-06-11 10:03:39 -0400

    Import assert_allclose from numpy.testing.

--- python/csparse/tests/test_cholesky.py
+++ python/csparse/tests/test_cholesky.py
@@ -12,6 +12,7 @@ Unit tests for the python Cholesky algorithms.
 import pytest
 import numpy as np
 
+from numpy.testing import assert_allclose
 from scipy import linalg as la
 from scipy import sparse
 from scipy.sparse import linalg as sla
@@ -49,12 +50,12 @@ def test_cholesky_interface(order, use_postorder):
     Ll = csparse.leftchol(A, order, use_postorder).L
     Lr = csparse.rechol(A, order, use_postorder).L
 
-    np.testing.assert_allclose(L.indptr, Ls.indptr)
-    np.testing.assert_allclose(L.indices, Ls.indices)
-    np.testing.assert_allclose(L.toarray(), Ll.toarray())
-    np.testing.assert_allclose(Ll.toarray(), Lr.toarray())
+    assert_allclose(L.indptr, Ls.indptr)
+    assert_allclose(L.indices, Ls.indices)
+    assert_allclose(L.toarray(), Ll.toarray())
+    assert_allclose(Ll.toarray(), Lr.toarray())
 
-    np.testing.assert_allclose((L @ L.T).toarray(), A[p][:, p].toarray(), atol=ATOL)
+    assert_allclose((L @ L.T).toarray(), A[p][:, p].toarray(), atol=ATOL)
 
 
 @pytest.mark.parametrize("chol_func", PYTHON_CHOL_FUNCS)
@@ -71,8 +72,8 @@ def test_python_cholesky(A, chol_func):
         L = chol_func(A, lower=True)
 
     # Check that algorithms work
-    np.testing.assert_allclose(L, L_, atol=ATOL)
-    np.testing.assert_allclose(L @ L.T, A, atol=ATOL)
+    assert_allclose(L, L_, atol=ATOL)
+    assert_allclose(L @ L.T, A, atol=ATOL)
 
 
 def test_python_cholesky_update(A):
@@ -102,24 +103,25 @@ def test_python_cholesky_update(A):
         A_up = A + wwT
 
         L_up, w_up = csparse.chol_update(L, w)
-        np.testing.assert_allclose(L_up @ L_up.T, A_up, atol=ATOL)
-        np.testing.assert_allclose(la.solve(L, w), w_up, atol=ATOL)
+        assert_allclose(L_up @ L_up.T, A_up, atol=ATOL)
+        assert_allclose(la.solve(L, w), w_up, atol=ATOL)
 
         L_upd, w_upd = csparse.chol_updown(L, w, update=True)
-        np.testing.assert_allclose(L_up, L_upd, atol=ATOL)
-        np.testing.assert_allclose(L_upd @ L_upd.T, A_up, atol=ATOL)
-        np.testing.assert_allclose(la.solve(L, w), w_upd, atol=ATOL)
+        assert_allclose(L_up, L_upd, atol=ATOL)
+        assert_allclose(L_upd @ L_upd.T, A_up, atol=ATOL)
+        assert_allclose(la.solve(L, w), w_upd, atol=ATOL)
 
         # Just downdate back to the original matrix!
         A_down = A.copy()  # A_down == A_up - wwT == A
         L_down, w_down = csparse.chol_downdate(L_up, w)
-        np.testing.assert_allclose(L_down @ L_down.T, A_down, atol=ATOL)
-        np.testing.assert_allclose(la.solve(L_up, w), w_down, atol=ATOL)
+        assert_allclose(L_down @ L_down.T, A_down, atol=ATOL)
+        assert_allclose(la.solve(L_up, w), w_down, atol=ATOL)
 
         L_downd, w_downd = csparse.chol_updown(L_up, w, update=False)
-        np.testing.assert_allclose(L_down, L_downd, atol=ATOL)
-        np.testing.assert_allclose(L_downd @ L_downd.T, A_down, atol=ATOL)
-        np.testing.assert_allclose(la.solve(L_up, w), w_downd, atol=ATOL)
+        assert_allclose(L_down, L_downd, atol=ATOL)
+        assert_allclose(L_downd @ L_downd.T, A_down, atol=ATOL)
+        assert_allclose(la.solve(L_up, w), w_downd, atol=ATOL)
+
 
 # -----------------------------------------------------------------------------
 #         Test 3
@@ -161,34 +163,34 @@ def test_trisolve_cholesky(problem):
     # Check lsolve
     x1 = sla.spsolve(L0, b)
     x2 = csparse.lsolve(L0, b)
-    np.testing.assert_allclose(x1, x2, atol=1e-12 * κ)
+    assert_allclose(x1, x2, atol=1e-12 * κ)
 
     # Check ltsolve
     x1 = sla.spsolve(L0.T, b)
     x2 = csparse.ltsolve(L0, b)
     err = la.norm(x1 - x2, ord=1)
-    np.testing.assert_allclose(x1, x2, atol=1e-10 * κ)
+    assert_allclose(x1, x2, atol=1e-10 * κ)
 
     # usolve
     U = L0.T
 
     x1 = sla.spsolve(U, b)
     x2 = csparse.usolve(U, b)
-    np.testing.assert_allclose(x1, x2, atol=1e-10 * κ)
+    assert_allclose(x1, x2, atol=1e-10 * κ)
 
     # utsolve (not in test3.m)
     x1 = sla.spsolve(U.T, b)
     x2 = csparse.utsolve(U, b)
-    np.testing.assert_allclose(x1, x2, atol=1e-10 * κ)
+    assert_allclose(x1, x2, atol=1e-10 * κ)
 
     # C++Sparse Cholesky
     L2 = csparse.chol(A).L
-    np.testing.assert_allclose(L0.toarray(), L2.toarray(), atol=1e-8 * κ)
+    assert_allclose(L0.toarray(), L2.toarray(), atol=1e-8 * κ)
 
     # Cholesky of re-ordered A
     L1 = sparse.csc_array(la.cholesky(C.toarray(), lower=True))
     L2 = csparse.chol(C).L
-    np.testing.assert_allclose(L1.toarray(), L2.toarray(), atol=1e-8 * κ)
+    assert_allclose(L1.toarray(), L2.toarray(), atol=1e-8 * κ)
 
     # Cholesky of A, then reorder
     res = csparse.chol(A, order=order)
@@ -196,7 +198,7 @@ def test_trisolve_cholesky(problem):
     p3 = res.p
     C3 = A[p3][:, p3]
     L4 = sparse.csc_array(la.cholesky(C3.toarray(), lower=True))
-    np.testing.assert_allclose(L3.toarray(), L4.toarray(), atol=1e-8 * κ)
+    assert_allclose(L3.toarray(), L4.toarray(), atol=1e-8 * κ)
     
 
 

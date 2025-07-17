bd41d3e959ed3391469d6f0d7eb499bb597aec32
Bernard Roesler <bernard.roesler@gmail.com>
2025-07-17 15:32:41 -0400

    Complete Exercise 7.7: dmperm timings with varying seed.

--- python/scripts/ex7.7_dmperm_perf.py
+++ python/scripts/ex7.7_dmperm_perf.py
@@ -19,17 +19,21 @@ example).
 """
 # =============================================================================
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import suitesparseget as ssg
 import timeit
 
 from functools import partial
 # from scipy import sparse
+from pathlib import Path
 from tqdm import tqdm
 
 import csparse
+
+pkl_file = Path('dmperm_perf.pkl')
 
 # Get the index
 ss_index = ssg.get_index()  # main database of matrices
@@ -52,40 +56,107 @@ sym_indef = sym_indef.drop(columns=cols_to_drop)
 # For each matrix, compute the time taken by cs_dmperm with different seeds
 SEEDS = {'reverse': -1, 'natural': 0, 'random': 1}
 
-df = pd.DataFrame(
-    index=sym_indef['id'],
-    columns=SEEDS.keys(),
+if pkl_file.exists():
+    df = pd.read_pickle(pkl_file)
+else:
+    df = pd.DataFrame(
+        index=sym_indef['id'],
+        columns=SEEDS.keys(),
+        dtype=float
+    )
+
+    N_repeats = 7  # default for %timeit == 7
+
+    for _idx, row in tqdm(sym_indef.iterrows(), total=len(sym_indef)):
+        try:
+            A = ssg.get_problem(index=ss_index, row=row).A
+        except Exception as e:
+            print(f"Error loading matrix {row['id']}: {e}")
+            continue
+
+        # Compute the dmperm orderings
+        for col, seed in tqdm(
+            SEEDS.items(),
+            desc=f"Processing {row['group']}/{row['name']}",
+            leave=False
+        ):
+            dm_func = partial(csparse.dmperm, A, seed=seed)
+
+            # Determine how many times to repeat the function
+            timer = timeit.Timer('dm_func()', globals={'dm_func': dm_func})
+            N_samples, _ = timer.autorange()
+            ts = timer.repeat(repeat=N_repeats, number=N_samples)
+            ts = np.array(ts) / N_samples
+
+            df.loc[row['id'], col] = np.min(ts)
+
+    # Compute the ratios of the times for each seed
+    df['natural/reverse'] = df['natural'] / df['reverse']
+    df['natural/random'] = df['natural'] / df['random']
+    df['random/reverse'] = df['random'] / df['reverse']
+
+    df.to_pickle('dmperm_perf.pkl')
+
+
+# -----------------------------------------------------------------------------
+#         Present Data
+# -----------------------------------------------------------------------------
+# Merge the results with the original index
+sym_indef = sym_indef.merge(df, on='id')
+
+# List top K and bottom K matrices for each seed
+K = 5
+timing_cols = ['natural', 'reverse', 'random']
+ratio_cols = ['natural/reverse', 'natural/random', 'random/reverse']
+
+for col in ratio_cols:
+    print(f"----- {col} ratios:")
+    print(f"Top {K} matrices:")
+    disp_cols = ['id', 'name', col]
+    print(sym_indef.nlargest(K, columns=[col])[disp_cols])
+
+    print(f"\nBottom {K} matrices:")
+    print(sym_indef.nsmallest(K, columns=[col])[disp_cols])
+
+# Plot histograms of each ratio
+# Melt the timing columns for easier plotting
+tf = (
+    df.reset_index()
+    .melt(
+        id_vars='id',
+        value_vars=ratio_cols,
+        var_name='seed',
+        value_name='time ratio',
+    )
 )
 
-N_repeats = 3  # default for %timeit == 7
+# Bin data on a log scale since we're expecting a log normal distribution
+# bins = np.logspace(np.log10(tf['time'].min()), np.log10(tf['time'].max()), 20)
 
-for _idx, row in tqdm(sym_indef.iloc[:3].iterrows(), total=len(sym_indef)):
-    try:
-        A = ssg.get_problem(index=ss_index, row=row).A
-    except Exception as e:
-        print(f"Error loading matrix {row['id']}: {e}")
-        continue
+fig, ax = plt.subplots(num=1, clear=True)
 
-    # Compute the dmperm orderings
-    for col, seed in tqdm(
-        SEEDS.items(),
-        desc=f"Processing {row['group']}/{row['name']}",
-        leave=False
-    ):
-        dm_func = partial(csparse.dmperm, A, seed=seed)
+# sns.histplot(
+#     tf,
+# 	x='time',
+# 	hue='seed',
+# 	bins=bins,
+# 	stat='probability',
+# 	multiple='stack',
+#     ax=ax
+# )
+# ax.set(xscale='log')
 
-        # Determine how many times to repeat the function
-        timer = timeit.Timer('dm_func()', globals={'dm_func': dm_func})
-        N_samples, _ = timer.autorange()
-        ts = timer.repeat(repeat=N_repeats, number=N_samples)
-        ts = np.array(ts) / N_samples
+sns.kdeplot(
+    tf,
+	x='time ratio',
+	hue='seed',
+    log_scale=True,
+    common_grid=True,
+    ax=ax
+)
 
-        df.loc[row['id'], col] = np.min(ts)
 
-# Compute the ratios of the times for each seed
-df['natural/reverse'] = df['natural'] / df['reverse']
-df['natural/random'] = df['natural'] / df['random']
-df['random/reverse'] = df['random'] / df['reverse']
+plt.show()
 
 # =============================================================================
 # =============================================================================

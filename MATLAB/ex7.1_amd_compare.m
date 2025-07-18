44ece380dd02a4bdfc0afef39a8c5f29dfb9200a
Bernard Roesler <bernard.roesler@gmail.com>
2025-07-18 13:18:23 -0400

    Update Exercise 7.1: AMD comparison.

--- MATLAB/ex7.1_amd_compare.m
+++ MATLAB/ex7.1_amd_compare.m
@@ -3,20 +3,25 @@
 %  Created: 2025-05-05 15:08
 %   Author: Bernie Roesler
 %
-%  Description: Compare the SuiteSparse AMD with the CSparse AMD function.
+%  Description: Solution to Davis, Exercise 7.1: Compare the built-in MATLAB AMD
+%  (`amd`), the SuiteSparse AMD (`amd2`), and the CSparse AMD (`cs_amd`).
+%
+%  Compare:
+%    - run time
+%    - memory usage
+%    - ordering quality (via number of non-zeros in the Cholesky factorization)
 %
 %===============================================================================
 
 clear;
 
-SAVE_FIGS = false;
+SAVE_FIGS = true;
 
 % Ns = [10, 20, 50, 100];
 Ns = [10, 20, 50, 100, 200, 500, 1000];
 % Ns = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000];
 density = 0.02;
 N_trials = 5;
-N_samples = 10;
 
 amd_times = zeros(length(Ns), 1);
 amd2_times = zeros(length(Ns), 1);
@@ -27,43 +32,61 @@ amd2_lnzs = zeros(length(Ns), 1);
 cs_amd_lnzs = zeros(length(Ns), 1);
 
 % Create a random sparse matrix and time row indexing
-for i = 1:length(Ns)
-    N = Ns(i);
-    disp(['N = ', num2str(N)]);
+for k = 1:length(Ns)
+    N = Ns(k);
+    disp(['----- N = ', num2str(N)]);
 
+    % Create a random symmetric matrix with specified bandwidth
     A = sprandsym(N, density);
-
-    % Specify bandwidth by eliminating elements outside bands
-    [J, I] = meshgrid(1:N, 1:N);
     bw = round(N/10);
-    is_within_band = abs(I - J) <= bw;
-    A(~is_within_band) = 0;
+    [i, j, v] = find(A);
+    keep = abs(i - j) <= bw;
+    A = sparse(i(keep), j(keep), v(keep), N, N);
 
-    amd_times(i) = timeit(@() amd(A), N_trials, N_samples);
-    amd2_times(i) = timeit(@() amd2(A), N_trials, N_samples);
-    cs_amd_times(i) = timeit(@() cs_amd(A), N_trials, N_samples);
+    amd_times(k) = timeit(@() amd(A), N_trials);
+    amd2_times(k) = timeit(@() amd2(A), N_trials);
+    cs_amd_times(k) = timeit(@() cs_amd(A), N_trials);
+
+    fprintf( ...
+        ['        AMD time: %.2e s\n', ...
+         '       AMD2 time: %.2e s\n', ...
+         'CSparse AMD time: %.2e s\n'], ...
+        amd_times(k), amd2_times(k), cs_amd_times(k) ...
+    );
 
     % Compare the number of non-zeros in the factorization
     p = amd(A);
-    amd_lnzs(i) = sum(symbfact(A(p,p)));
+    amd_lnzs(k) = sum(symbfact(A(p,p)));
 
     p = amd2(A);
-    amd2_lnzs(i) = sum(symbfact(A(p,p)));
+    amd2_lnzs(k) = sum(symbfact(A(p,p)));
 
     p = cs_amd(A);
-    cs_amd_lnzs(i) = sum(symbfact(A(p,p)));
+    cs_amd_lnzs(k) = sum(symbfact(A(p,p)));
+
+    amd_titles = {'Built-In AMD', 'SuiteSparse AMD', 'CSparse AMD'};
+    amd_funcs = { ...
+        @(A) amd(A), ...
+        @(A) amd2(A), ...
+        @(A) cs_amd(A) ...
+    };
 
     % Display a matrix
-    if N == 1000
+    if N == max(Ns)
         figure(3); clf;
 
-        subplot(1, 2, 1);
+        subplot(1, 4, 1);
         spy(A);
         title('Matrix A');
+        axis equal;
 
-        subplot(1, 2, 2);
-        spy(A(p,p));
-        title('AMD Permuted Matrix');
+        for s = 1:3
+            subplot(1, 4, s + 1);
+            p = amd_funcs{s}(A);
+            spy(A(p, p));
+            title(amd_titles{s});
+            axis equal;
+        end
 
         orient landscape;
 

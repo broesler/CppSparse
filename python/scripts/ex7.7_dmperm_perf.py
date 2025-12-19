b708b160e658691ab399c0cea2a97688bc11ee66
Bernard Roesler <bernard.roesler@gmail.com>
2025-12-19 11:14:37 -0500

    Linting on Exercise 7.7 script.

--- python/scripts/ex7.7_dmperm_perf.py
+++ python/scripts/ex7.7_dmperm_perf.py
@@ -21,8 +21,6 @@ example).
 
 import timeit
 from functools import partial
-
-# from scipy import sparse
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -34,37 +32,33 @@ from tqdm import tqdm
 
 import csparse
 
-pkl_file = Path('dmperm_perf.pkl')
+FORCE_UPDATE = False
+
+pkl_file = Path("dmperm_perf.pkl")
 
 # Get the index
 ss_index = ssg.get_index()  # main database of matrices
 ss_stats = ssg.get_stats()  # additional matrix info
 
 # Filter for symmetric indefinite matrices
-conditions = (ss_index['numerical_symmetry'] == 1.0) & (~ss_index['posdef'])
+conditions = (ss_index["numerical_symmetry"] == 1.0) & (~ss_index["posdef"])
 
-ghs_indef = ss_index.loc[ss_index['group'] == 'GHS_indef']  # 60 matrices
-sym_indef = (
-    # ss_index.loc[conditions]
-    ghs_indef.loc[conditions]
-    .merge(ss_stats, on='id', suffixes=(None, '_stats'))
+ghs_indef = ss_index.loc[ss_index["group"] == "GHS_indef"]  # 60 matrices
+sym_indef = ghs_indef.loc[conditions].merge(
+    ss_stats, on="id", suffixes=(None, "_stats")
 )
 
 # Drop the duplicate '_stats' columns
-cols_to_drop = sym_indef.filter(like='_stats').columns
+cols_to_drop = sym_indef.filter(like="_stats").columns
 sym_indef = sym_indef.drop(columns=cols_to_drop)
 
 # For each matrix, compute the time taken by cs_dmperm with different seeds
-SEEDS = {'reverse': -1, 'natural': 0, 'random': 1}
+SEEDS = {"reverse": -1, "natural": 0, "random": 1}
 
-if pkl_file.exists():
+if not FORCE_UPDATE and pkl_file.exists():
     df = pd.read_pickle(pkl_file)
 else:
-    df = pd.DataFrame(
-        index=sym_indef['id'],
-        columns=SEEDS.keys(),
-        dtype=float
-    )
+    df = pd.DataFrame(index=sym_indef["id"], columns=SEEDS.keys(), dtype=float)
 
     N_repeats = 7  # default for %timeit == 7
 
@@ -79,7 +73,7 @@ else:
         for col, seed in tqdm(
             SEEDS.items(),
             desc=f"Processing {row['group']}/{row['name']}",
-            leave=False
+            leave=False,
         ):
             dm_func = partial(csparse.dmperm, A, seed=seed)
 
@@ -89,31 +83,31 @@ else:
             ts = timer.repeat(repeat=N_repeats, number=N_samples)
             ts = np.array(ts) / N_samples
 
-            df.loc[row['id'], col] = np.min(ts)
+            df.loc[row["id"], col] = np.min(ts)
 
     # Compute the ratios of the times for each seed
-    df['natural/reverse'] = df['natural'] / df['reverse']
-    df['natural/random'] = df['natural'] / df['random']
-    df['random/reverse'] = df['random'] / df['reverse']
+    df["natural/reverse"] = df["natural"] / df["reverse"]
+    df["natural/random"] = df["natural"] / df["random"]
+    df["random/reverse"] = df["random"] / df["reverse"]
 
-    df.to_pickle('dmperm_perf.pkl')
+    df.to_pickle("dmperm_perf.pkl")
 
 
 # -----------------------------------------------------------------------------
 #         Present Data
 # -----------------------------------------------------------------------------
 # Merge the results with the original index
-sym_indef = sym_indef.merge(df, on='id')
+sym_indef = sym_indef.merge(df, on="id")
 
 # List top K and bottom K matrices for each seed
 K = 5
-timing_cols = ['natural', 'reverse', 'random']
-ratio_cols = ['natural/reverse', 'natural/random', 'random/reverse']
+timing_cols = ["natural", "reverse", "random"]
+ratio_cols = ["natural/reverse", "natural/random", "random/reverse"]
 
 for col in ratio_cols:
     print(f"----- {col} ratios:")
     print(f"Top {K} matrices:")
-    disp_cols = ['id', 'name', col]
+    disp_cols = ["id", "name", col]
     print(sym_indef.nlargest(K, columns=[col])[disp_cols])
 
     print(f"\nBottom {K} matrices:")
@@ -121,40 +115,32 @@ for col in ratio_cols:
 
 # Plot histograms of each ratio
 # Melt the timing columns for easier plotting
-tf = (
-    df.reset_index()
-    .melt(
-        id_vars='id',
-        value_vars=ratio_cols,
-        var_name='seed',
-        value_name='time ratio',
-    )
+tf = df.reset_index().melt(
+    id_vars="id",
+    value_vars=ratio_cols,
+    var_name="seed",
+    value_name="time ratio",
 )
 
 # Bin data on a log scale since we're expecting a log normal distribution
-# bins = np.logspace(np.log10(tf['time'].min()), np.log10(tf['time'].max()), 20)
-
 fig, ax = plt.subplots(num=1, clear=True)
+
+# bins = np.logspace(
+#     np.log10(tf["time ratio"].min()), np.log10(tf["time ratio"].max()), 20
+# )
 
 # sns.histplot(
 #     tf,
-#   x='time',
-#   hue='seed',
-#   bins=bins,
-#   stat='probability',
-#   multiple='stack',
-#     ax=ax
+#     x="time ratio",
+#     hue="seed",
+#     bins=bins,
+#     stat="probability",
+#     multiple="stack",
+#     ax=ax,
 # )
-# ax.set(xscale='log')
+# ax.set(xscale="log")
 
-sns.kdeplot(
-    tf,
-    x='time ratio',
-    hue='seed',
-    log_scale=True,
-    common_grid=True,
-    ax=ax
-)
+sns.kdeplot(tf, x="time ratio", hue="seed", log_scale=True, common_grid=True, ax=ax)
 
 
 plt.show()

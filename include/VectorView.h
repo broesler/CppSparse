68156224a8678ffba1bf2c0e83a3e54171ed8233
Bernard Roesler <bernard.roesler@gmail.com>
2026-08-14 22:26:25 -0400

    ENH: Initial implementation of VectorView.

---
+++ include/VectorView.h
@@ -0,0 +1,161 @@
+//==============================================================================
+//     File: VectorView.h
+//  Created: 2026-08-14 20:53
+//   Author: Bernie Roesler
+//
+//  Description: Define the VectorView class.
+//
+//==============================================================================
+
+#pragma once
+
+#include <algorithm>        // transform
+#include <concepts>         // integral, floating_point
+#include <format>           // format
+#include <functional>       // plus, etc.
+#include <initializer_list>
+#include <ranges>           // from_range_t, views::transform
+#include <stdexcept>        // invalid_argument
+#include <type_traits>      // is_same_v
+#include <iterator>         // const_iterator
+
+#include "Vector.h"
+
+
+namespace cs {
+
+
+/// A class that provides a contiguous view onto a Vector.
+template <Arithmetic T>
+class VectorView
+{
+public:
+
+    // -------------------------------------------------------------------------
+    //         STL Container Type Aliases
+    // -------------------------------------------------------------------------
+    using element_type = T;
+    using value_type = std::remove_cv_t<T>;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
+    using iterator = typename std::span<T>::iterator;
+    using const_iterator = typename std::span<std::add_const_t<T>>::iterator;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    // -------------------------------------------------------------------------
+    //         Constructors
+    // -------------------------------------------------------------------------
+    VectorView() = default;
+
+    template <std::contiguous_iterator It>
+    VectorView(It first, size_t count) : data_(first, count) {}
+
+    template <std::contiguous_iterator It, std::sized_sentinel_for<It> End>
+    VectorView(It first, End last) : data_(first, last) {}
+
+    // NOTE the "requires" line prevents "hijacking" the copy constructor
+    template <std::ranges::contiguous_range R>
+    requires (!std::is_same_v<std::remove_cvref_t<R>, VectorView>) 
+    VectorView(R&& range) : data_(std::forward<R>(range)) {}
+
+    VectorView(const VectorView&) = default;
+    VectorView(VectorView&&) = default;
+    ~VectorView() = default;
+
+    VectorView& operator=(const VectorView&) = default;
+    VectorView& operator=(VectorView&&) = default;
+
+    // -------------------------------------------------------------------------
+    //         Iterators
+    // -------------------------------------------------------------------------
+    auto begin() noexcept { return data_.begin(); }
+    auto end() noexcept { return data_.end(); }
+
+    auto begin() const noexcept { return data_.begin(); }
+    auto end() const noexcept { return data_.end(); }
+
+    auto cbegin() const noexcept { return data_.cbegin(); }
+    auto cend() const noexcept { return data_.cend(); }
+
+    auto rbegin() noexcept { return data_.rbegin(); }
+    auto rend() noexcept { return data_.rend(); }
+
+    auto rbegin() const noexcept { return data_.rbegin(); }
+    auto rend() const noexcept { return data_.rend(); }
+
+    auto crbegin() const noexcept { return data_.crbegin(); }
+    auto crend() const noexcept { return data_.crend(); }
+
+    // -------------------------------------------------------------------------
+    //         Element Access
+    // -------------------------------------------------------------------------
+    T& front() const { return data_.front(); }
+    T& back() const { return data_.back(); }
+    // T& at(size_t i) const { return data_.at(i); }  // C++26 only
+    T& operator[](size_t i) const { return data_[i]; }
+    T* data() const noexcept { return data_.data(); }
+
+    // -------------------------------------------------------------------------
+    //         Observers
+    // -------------------------------------------------------------------------
+    size_t size() const noexcept { return data_.size(); }
+    bool empty() const noexcept { return data_.empty(); }
+
+    // -------------------------------------------------------------------------
+    //         Subviews
+    // -------------------------------------------------------------------------
+    VectorView first(size_t count) const { return VectorView(data_.first(count)); }
+    VectorView last(size_t count) const { return VectorView(data_.last(count)); }
+    VectorView subspan(size_t offset, size_t count = std::dynamic_extent) const {
+        return VectorView(data_.subspan(offset, count));
+    }
+
+    // -------------------------------------------------------------------------
+    //         Comparison Operators
+    // -------------------------------------------------------------------------
+    constexpr bool operator==(const VectorView& rhs) const {
+        return std::ranges::equal((*this), rhs);
+    }
+
+    // -------------------------------------------------------------------------
+    //         Operators
+    // -------------------------------------------------------------------------
+    // Vector-Vector
+    VectorView& operator+=(VectorView<const T> rhs) { return apply_elementwise_(rhs, std::plus<>()); }
+    VectorView& operator-=(VectorView<const T> rhs) { return apply_elementwise_(rhs, std::minus<>()); }
+    VectorView& operator*=(VectorView<const T> rhs) { return apply_elementwise_(rhs, std::multiplies<>()); }
+    VectorView& operator/=(VectorView<const T> rhs) { return apply_elementwise_(rhs, std::divides<>()); }
+
+private:
+    std::span<T> data_;
+
+    void check_same_size_(VectorView<const T> rhs) const {
+        if (size() != rhs.size()) {
+            throw std::invalid_argument(
+                std::format("Vector size mismatch: {} vs {}", size(), rhs.size())
+            );
+        }
+    }
+
+    template <typename BinaryOp>
+    VectorView& apply_elementwise_(VectorView<const T> rhs, BinaryOp op) {
+        check_same_size_(rhs);
+        std::ranges::transform((*this), rhs, begin(), op);
+        return *this;
+    }
+
+};  // VectorView
+
+
+using VectorViewD = VectorView<double>;
+
+
+}  // namespace cs
+
+//==============================================================================
+//==============================================================================
---
+++ test/test_vector_view.cpp
@@ -0,0 +1,113 @@
+/*==============================================================================
+ *     File: test/test_vector_view.cpp
+ *  Created: 2026-08-14 21:06
+ *   Author: Bernie Roesler
+ *
+ *  Description: Test CSparse VectorView class
+ *
+ *============================================================================*/
+
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
+
+#include <ranges>  // from_range
+
+#include "Vector.h"
+#include "VectorView.h"
+
+
+namespace cs {
+
+TEST_CASE("VectorView Construction", "[vectorview][basic]")
+{
+    SECTION("Default constructor") {
+        VectorViewD a;
+        REQUIRE(a.size() == 0);
+        REQUIRE(a.empty());
+    }
+
+    SECTION("Vector constructor") {
+        VectorD x{1, 2, 3};
+        VectorViewD a(x);
+        REQUIRE(a.size() == 3);
+        REQUIRE(!a.empty());
+        for (size_t i = 0; i < a.size(); ++i) {
+            CAPTURE(i);
+            REQUIRE(a[i] == static_cast<double>(i + 1));
+        }
+        REQUIRE(a.front() == 1);
+        REQUIRE(a.back() == 3);
+    }
+}
+
+
+TEST_CASE("Vector Operators", "[vectorview][ops]")
+{
+    VectorD a{1, 2, 3};
+
+    SECTION("operator==") {
+        VectorViewD a_view(a);
+        VectorViewD b_view(a);
+        REQUIRE(a_view == b_view);
+        REQUIRE_FALSE(a_view != b_view);
+    }
+
+    SECTION("operator+=") {
+        VectorD b{4, 5, 6};
+        VectorViewD a_view(a);
+        VectorViewD b_view(b);
+
+        b_view += a_view;
+
+        VectorD expect {5, 7, 9};
+        VectorViewD expect_view(b);
+
+        REQUIRE(b_view == expect_view);
+    }
+
+    SECTION("operator-=") {
+        VectorD b{4, 6, 8};
+        VectorViewD a_view(a);
+        VectorViewD b_view(b);
+
+        b_view -= a_view;
+
+        VectorD expect {3, 4, 5};
+        VectorViewD expect_view(b);
+
+        REQUIRE(b_view == expect_view);
+    }
+
+    SECTION("operator*=") {
+        VectorD b{4, 5, 6};
+        VectorViewD a_view(a);
+        VectorViewD b_view(b);
+
+        b_view *= a_view;
+
+        VectorD expect {4, 10, 18};
+        VectorViewD expect_view(b);
+
+        REQUIRE(b_view == expect_view);
+    }
+
+    SECTION("operator/=") {
+        VectorD b{4, 5, 6};
+        VectorViewD a_view(a);
+        VectorViewD b_view(b);
+
+        b_view /= a_view;
+
+        VectorD expect {4, 2.5, 2};
+        VectorViewD expect_view(b);
+
+        REQUIRE(b_view == expect_view);
+    }
+}
+
+
+}  // namespace cs
+
+/*==============================================================================
+ *============================================================================*/
